22c3defc463447fe6faf7c529339c35b548dd265
brayan <brayan_su@tech.edu.mx>
2022-11-29 15:06:04 -0600

    modificacion en rutas

--- rutas/usuarioRutas.js
+++ rutas/usuarioRutas.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var UsuarioControl = require('../controlador/usuarioControlador');
 
+
+//hola soy brayan
 var api = express.Router();
 
 api.get('/probando-controlador', UsuarioControl.prueba);
